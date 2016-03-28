c64226ddbbb335e5f38810cfb5258da1fc642048
Mark Stanley <stanleymark92a@aol.com>
2016-03-28 14:35:23 -0400

    Delete group.js

--- Freetime-Web-App/app/models/group.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose'); 
-var groupSchema = mongoose.Schema({ 
-		name: String, 
-		members: [String],
-		meetings:[String]
-}); 
-module.exports = mongoose.model('Group', groupSchema);
