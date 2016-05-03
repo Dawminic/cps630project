449d63f8b882c2a94604e6e7173a67e7ce8af09c
Dawminic <dominic.aidoo@gmail.com>
2016-05-03 19:28:28 -0400

    Deleted folder containing node modules

--- Freetime/config/auth.js
+++ Freetime/config/auth.js
@@ -2,9 +2,10 @@
 // expose our config directly to our application using module.exports
 module.exports = {
     'googleAuth' : {
-        'clientID'      : '145217622872-45mhibnelavga610sog01k2lkc87f9ab.apps.googleusercontent.com',
-        'clientSecret'  : 'aDlsp2gSzBSIMoHyUdaW9Evk',
-        'callbackURL'   : 'http://localhost:8000/auth/google/callback'
+        'clientID'      : '813433863993-dn497fmkof7a11h31rt6n7e1tubm2t68.apps.googleusercontent.com',
+        'clientSecret'  : 'gkBwdU3x-Lh_fG5b_juOVsn4',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback',
+        'proxy': true
     }
 
 };
