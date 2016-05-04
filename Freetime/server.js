2392befe2c3a1e6dce6dc7c4dcc620b226b81887
Dawminic <dominic.aidoo@gmail.com>
2016-05-03 21:07:14 -0400

    "Updated files"

--- Freetime/server.js
+++ Freetime/server.js
@@ -22,7 +22,8 @@ require('./config/passport.js')(passport);
 //initialize app
 var app = express();
 //set port to 8000 ---TAKE OUT LATER WHEN
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.use("/public",express.static(__dirname + "/public"));
 app.use(morgan('dev'));
@@ -38,5 +39,5 @@ app.use(flash());
 app.set('view engine','ejs');
 require('./app/routes.js')(app,passport);
 
-app.listen(port);
-console.log('Server running on port: ' + port);
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
